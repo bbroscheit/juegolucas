0cc5002d1714321979c5a7bab9aacdab56714851
bbroscheit <96868981+bbroscheit@users.noreply.github.com>
2022-06-14 09:25:37 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,127 @@
+const enemy = [
+    { 
+        name:"Lancelot",
+        health:100,
+        attack:10,
+        defense:8,
+        counterAttack:4,
+        img:"assets/images/lancelot.jpg"
+    },
+    {
+        name:"Gawain",
+        health:100,
+        attack:8,
+        defense:5,
+        counterAttack:8,
+        img:"assets/images/gawain.jpg"
+    },
+    {
+        name:"Arthur",
+        health:150,
+        attack:12,
+        defense:6,
+        counterAttack:10,
+        img:"assets/images/arthur.png"
+    },
+    {
+        name:"Galahad",
+        health:80,
+        attack:5,
+        defense:10,
+        counterAttack:3,
+        img:"assets/images/galahad.jpg"
+    }
+
+
+]
+
+let player = {
+    name:"",
+    health:100,
+    attack:0,
+    defense:0,
+    counterAttack:0,
+    img:""
+}
+
+let enemyNum = Math.floor(Math.random() * 3);
+
+document.getElementById("createMc").addEventListener("click", function(){
+    player.name = prompt("What is your name?");
+    player.attack =Math.floor(Math.random() * 15) + 1;
+    player.defense = Math.floor(Math.random() * 10) + 1;
+    player.counterAttack = Math.floor(Math.random() * 10) + 1;
+    document.getElementById("mcName").innerHTML = player.name;
+    document.getElementById("mcHealth").innerHTML = "HP: " + player.health;
+    document.getElementById("mcAttack").innerHTML = "Attack: " +player.attack;
+    document.getElementById("mcDefense").innerHTML = "Defense: " + player.defense;
+    document.getElementById("mcCounterAttack").innerHTML = "CounterAttack: "+player.counterAttack;
+    
+    
+
+    document.getElementById("enemyName").innerHTML = enemy[enemyNum].name;
+    document.getElementById("enemyHealth").innerHTML = "HP: "+enemy[enemyNum].health;
+    document.getElementById("enemyAttack").innerHTML = "Attack: "+enemy[enemyNum].attack;
+    document.getElementById("enemyDefense").innerHTML = "Defense: "+enemy[enemyNum].defense;
+    document.getElementById("enemyCounterAttack").innerHTML = "CounterAttack: "+enemy[enemyNum].counterAttack;
+    document.getElementById("enemyImg").src = enemy[enemyNum].img;
+});
+
+let round = 0;
+const attack = document.getElementById("attack");
+    attack.addEventListener("click", function(){
+        round = 0;
+        while(player.health > 0 && enemy[enemyNum].health > 0){
+        if  (round === 0){ 
+            if(Math.floor(Math.random()*7) > 4){
+                
+                enemy[enemyNum].health -= player.attack;
+                document.getElementById("enemyHealth").innerHTML = enemy[enemyNum].health;
+                let attacker = true;
+                alert("You attacked the enemy!");
+                
+                    if(attacker === true && Math.floor(Math.random()*7) > 4 ){
+                        
+                        player.health -= enemy[enemyNum].counterAttack;
+                        document.getElementById("mcHealth").innerHTML = player.health;
+                        alert("The enemy counterAttack you!");
+                    }else{
+                        alert("You Block the enemy's attack!");
+                    }
+                
+            }else{
+                alert("You missed!");
+            }
+
+            if(Math.floor(Math.random()*7) > 4){
+                
+                player.health -= enemy[enemyNum].attack;
+                document.getElementById("mcHealth").innerHTML = player.health;
+                let attackerEnemy = true;
+                alert("The enemy attacked you!");
+                    if(attackerEnemy === true && Math.floor(Math.random()*7) > 4 ){
+                        
+                        enemy[enemyNum].health -= player.counterAttack;
+                        document.getElementById("enemyHealth").innerHTML = enemy[enemyNum].health;
+                        alert("You counterAttack the enemy!");
+                    }else{
+                        alert("Enemy Block your attack!");
+                    }
+            }else{
+                alert("The enemy missed!");
+            }
+            round ++;
+        }
+       break 
+    } 
+    if(player.health <= 0){
+        alert("You lose!");
+    }else if(enemy[enemyNum].health <= 0){
+        alert("You win!");
+    }
+});
+
+
+
+
+
